aa6ac2078b95d48f0acdc72541a3b6d1b6f2156b
Brian C. Lindner <brian@bclindner.com>
2018-11-05 18:57:51 -0500

    improved logging for PingPongCommand

--- pingpong.go
+++ pingpong.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/bwmarrin/discordgo"
 	"strings"
+	log "github.com/sirupsen/logrus" // logging suite
 )
 
 type PingPongCommand struct {
+	Name string
 	PingString string
 	PongString string
 }
@@ -14,5 +16,11 @@ func (p PingPongCommand) Test(bot *discordgo.Session, evt *discordgo.MessageCrea
 	return strings.Contains(evt.Message.Content, p.PingString)
 }
 func (p PingPongCommand) Run(bot *discordgo.Session, evt *discordgo.MessageCreate) {
+	author := *evt.Message.Author
+	log.WithFields(log.Fields{
+		"command": p.Name,
+		"userID": author.ID,
+		"username": author.Username + "#" + author.Discriminator,
+	}).Info("sending pong")
 	bot.ChannelMessageSend(evt.Message.ChannelID, p.PongString)
 }
