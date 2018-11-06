417179445eae6d64eaec3d81d6c5b39f9a59565b
Brian C. Lindner <brian@bclindner.com>
2018-11-05 19:09:15 -0500

    improved structure and commenting

--- valerius.go
+++ valerius.go
@@ -11,18 +11,22 @@ import (
 )
 
 func init() {
-	// setup logging
+	// setup logging file
 	logfile, err := os.OpenFile("valerius.log", os.O_WRONLY | os.O_CREATE, 0644)
 	if err != nil { log.Fatal("Unable to establish logging: ",err) }
+	// set up the output and formatter
 	log.SetOutput(io.MultiWriter(os.Stdout, logfile))
 	log.SetFormatter(&log.JSONFormatter{})
 }
 
+// Structure for the bot configuration JSON file.
 type BotConfiguration struct {
 	BotToken string `json:"botToken"`
 }
 
-func initBot() (bot *discordgo.Session, err error) {
+// Initialize the bot.
+func initBot() (bot *discordgo.Session, user *discordgo.User, err error) {
+	log.Info("Bot initializing")
 	// setup logrus config
 	// load bot config file
 	configFile, err := ioutil.ReadFile("valerius.json")
@@ -34,17 +38,17 @@ func initBot() (bot *discordgo.Session, err error) {
 	// initialize the bot
 	bot, err = discordgo.New("Bot "+config.BotToken)
 	bot.Open()
+	// get the current bot user
+	user, err = bot.User("@me")
+	if err != nil { return }
+	log.Info("Bot logged in as ",user.Username,"#",user.Discriminator)
 	return
 }
 
 func main() {
 	// initialize the bot
-	log.Info("Bot initializing")
-	bot, err := initBot()
+	bot, user, err := initBot()
 	if err != nil { log.Fatal("Failed to initialize bot: ",err) }
-	// get the current bot user
-	user, err := bot.User("@me")
-	log.Info("Bot logged in as ",user.Username,"#",user.Discriminator)
 	// instantiate and register the handler
 	handler := NewMessageHandler(bot, user)
 	// add handler commands
